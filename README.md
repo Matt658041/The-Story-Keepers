a18b9354b698d298c12beabc728ba94657b1c99c
Kimberly West <kdenton993@gmail.com>
2022-05-19 20:42:04 -0600

    added ratings table

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # The-Story-Keepers
 A  gathering place for book lovers to search for, post about, and vote for their favorite books.
+
+##
--- db/schema.sql
+++ db/schema.sql
@@ -2,5 +2,14 @@ DROP DATABASE IF EXISTS story_keepers_db;
 
 CREATE DATABASE story_keepers_db;
 
-Table users (id (primary key), username, password, email)
-Table rating (rated_by_user (foreign key), rated_user (foreign key), rating)
+create table users (
+    id integer auto_increment primary key,
+    username VARCHAR(100),
+    password VARCHAR(100),
+    email VARCHAR(100)
+);
+
+create table rating (
+    rated_by_user integer,
+    rating integer
+);
