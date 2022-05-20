9d412d184b113c0982a496fba07021cde122cd8d
Kimberly West <kdenton993@gmail.com>
2022-05-19 19:45:28 -0600

    tables in sql

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,6 @@
 DROP DATABASE IF EXISTS story_keepers_db;
 
 CREATE DATABASE story_keepers_db;
+
+Table users (id (primary key), username, password, email)
+Table rating (rated_by_user (foreign key), rated_user (foreign key), rating)
