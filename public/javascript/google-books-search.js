274682145fd790d54a38790ae25b04d7d89487c2
Matt658041 <91630948+Matt658041@users.noreply.github.com>
2022-05-23 14:05:31 -0500

    added body-parser to google book js

--- public/javascript/google-books-search.js
+++ public/javascript/google-books-search.js
@@ -1,11 +1,32 @@
 const books = require('google-books-search');
+const bodyParser = require('body-parser');
 
-books.search('Enders game', function(error, results) {
-    if ( ! error ) {
-        console.log(results);
+
+
+async function newFormHandler(event) {
+    event.preventDefault();
+  
+    const title = document.querySelector('input[name="google-books-search"]').value;
+    const post_url = document.querySelector('input[name="google-books-search"]').value;
+  
+    const response = await fetch(`/api/posts`, {
+      method: 'POST',
+      body: JSON.stringify({
+        title,
+        post_url
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+  
+    if (response.ok) {
+      document.location.replace('/dashboard');
     } else {
-        console.log(error);
+      alert(response.statusText);
     }
-});
+  }
+  
+  document.querySelector('.new-post-form').addEventListener('submit', newFormHandler);
 
 
