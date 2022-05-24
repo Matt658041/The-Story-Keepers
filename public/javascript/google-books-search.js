55885f20147d33fd70710a8566fa2143ca2e5258
Nathan Shattuck <Nshattuck96@gmail.com>
2022-05-23 21:04:15 -0400

    update

--- public/javascript/google-books-search.js
+++ public/javascript/google-books-search.js
@@ -1,11 +1,3 @@
-const books = require('google-books-search');
 
-books.search('Enders game', function(error, results) {
-    if ( ! error ) {
-        console.log(results);
-    } else {
-        console.log(error);
-    }
-});
 
 
