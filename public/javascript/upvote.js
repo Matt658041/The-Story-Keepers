a99892a21caa9bf010c2f26a5f47d4aa80266d01
Kimberly West <kdenton993@gmail.com>
2022-05-23 19:05:00 -0600

    add functions for upvote ratings

--- public/javascript/upvote.js
+++ public/javascript/upvote.js
@@ -1,6 +1,4 @@
-async function upvoteClickHandler(event) {
-  event.preventDefault();
-
+function executeUpvote(rating) {
   const id = window.location.toString().split('/')[
     window.location.toString().split('/').length - 1
   ];
@@ -19,6 +17,35 @@ async function upvoteClickHandler(event) {
   } else {
     alert(response.statusText);
   }
+
 }
 
-document.querySelector('.upvote-btn').addEventListener('click', upvoteClickHandler);
+async function upvote1ClickHandler(event) {
+  event.preventDefault();
+executeUpvote(1);
+}
+async function upvote1ClickHandler(event) {
+  event.preventDefault();
+executeUpvote(1);
+}
+async function upvote2ClickHandler(event) {
+  event.preventDefault();
+executeUpvote(2);
+}
+async function upvote3ClickHandler(event) {
+  event.preventDefault();
+executeUpvote(3);
+}async function upvote4ClickHandler(event) {
+  event.preventDefault();
+executeUpvote(4);
+}
+async function upvote5ClickHandler(event) {
+  event.preventDefault();
+executeUpvote(5);
+}
+
+document.querySelector('.upvote-btn1').addEventListener('click', upvoteClickHandler);
+document.querySelector('.upvote-btn2').addEventListener('click', upvoteClickHandler);
+document.querySelector('.upvote-btn3').addEventListener('click', upvoteClickHandler);
+document.querySelector('.upvote-btn4').addEventListener('click', upvoteClickHandler);
+document.querySelector('.upvote-btn5').addEventListener('click', upvoteClickHandler);
